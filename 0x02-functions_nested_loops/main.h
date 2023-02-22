3e28252b4d495b53b199822a230c2d3d257da237
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2023-02-22 02:38:57 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#ifndef _holberton_h_
-#define _holberton_h_
+#ifndef main_h
+#define main_h
+
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,6 +14,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
