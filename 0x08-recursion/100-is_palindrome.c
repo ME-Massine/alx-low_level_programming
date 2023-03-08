c6b6b96c2e630fc6db67d186080642e66adb9603
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2023-03-08 23:50:01 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,16 +14,8 @@ int is_palindrome(char *s) {
 int main(void) {
     int r;
 
-    r = is_palindrome("");
+    r = is_palindrome("abcddcba");
     printf("%d\n", r);
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("redder");
-    printf("%d\n", r);
-    r = is_palindrome("test");
-    printf("%d\n", r);
-    r = is_palindrome("step on no pets");
-    printf("%d\n", r);
-    
+
     return 0;
 }
