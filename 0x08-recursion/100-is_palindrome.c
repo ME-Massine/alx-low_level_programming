501c0fbc6482cde17fe0c120569d901a37e2735b
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2023-03-08 23:51:54 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
-int is_palindrome(char *s) {
-    int len = strlen(s);
-    for (int i = 0; i < len/2; i++) {
-        if (s[i] != s[len-1-i]) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-int main(void) {
-    int r;
-
-    r = is_palindrome("abcddcba");
-    printf("%d\n", r);
-
-    return 0;
+/**
+ * wildcmp - compares two strings.
+ * @s1: string 1.
+ * @s2: string 2. It can contains a * as a special character.
+ * Return: 1 if are identical, 0 if not.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
