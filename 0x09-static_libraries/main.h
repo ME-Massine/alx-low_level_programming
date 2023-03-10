5582b51e4a839fde73e5da47cdd1edcad36b9a96
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2023-03-10 23:40:15 +0100

    Add files via upload

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-		
-int _putchar(char c);		
+
+/*
+ *  * File: main.h
+ *   * Auth: Alagwu David
+ *    * Desc: Header file containing the prototypes of all functions
+ *     *       included in the static library libholberton.a.
+ *      */
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -21,4 +27,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
